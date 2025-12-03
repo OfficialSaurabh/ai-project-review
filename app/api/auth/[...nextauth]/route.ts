2f7c031bed76d9bc36a9d8106f9b48b44e91b2df
Saurabh Kumar <saurabh.kumar@indusnet.co.in>
2025-12-03 15:24:57 +0530

    Nav bar and login done

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,13 +2,37 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import type { NextAuthOptions } from "next-auth";
 
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string;
+  }
+}
+
 export const authOptions: NextAuthOptions = {
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
+       authorization: {
+        params: {
+          scope: "read:user user:email repo",
+        },
+      },
     }),
   ],
+   callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      session.accessToken = typeof token.accessToken === "string" ? token.accessToken : undefined;
+      return session;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
