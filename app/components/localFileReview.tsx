aef32b80e0b190c5c1aed526b1e04cb1b4d16e74
Saurabh Kumar <saurabh.kumar@indusnet.co.in>
2025-12-17 17:40:33 +0530

    local review added

--- app/components/localFileReview.tsx
+++ app/components/localFileReview.tsx
@@ -91,7 +91,7 @@ export default function LocalFileReview({ setReviewLocalFile }: LocalFileReviewP
     setResult(null);
 
     try {
-      const res = await fetch("http://localhost:8000/review", {
+      const res = await fetch("http://127.0.0.1:8000/review", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
